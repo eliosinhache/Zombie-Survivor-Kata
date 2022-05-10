16cbf7f2b01935dddd284167dee557fa4bf12ad8
eliosinhache <elioh93@gmail.com>
2022-05-09 23:06:22 -0300

    Initial Project and Step One: Survivors

---
+++ Assets/Scripts/Classes/Survivor.cs
@@ -0,0 +1,30 @@
+﻿namespace Classes
+{
+    public class Survivor
+    {
+        public string name;
+        public int wounds;
+        public bool isAlive;
+        public int actions;
+
+        public Survivor(string survivorName)
+        {
+            name = survivorName;
+            isAlive = true;
+            actions = 3;
+            wounds = 0;
+        }
+
+        public void ReceiveWound()
+        {
+            if (isAlive)
+            {
+                if (wounds < 2)
+                {
+                    wounds += 1;
+                    isAlive = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/SurvivorShould.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Classes;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SurvivorShould
+    {
+        private Survivor _survivor;
+        [SetUp]
+        public void SetUp()
+        {
+            _survivor = new Survivor("Juan");
+        }
+        
+        [Test]
+        public void HasAName()
+        {
+            Assert.AreEqual("Juan", _survivor.name);
+        }
+        
+        [Test]
+        public void StartWithNoWounds()
+        {
+            Assert.AreEqual(0, _survivor.wounds);
+        }
+        
+        [Test]
+        public void DieWithTwoWounds()
+        {
+            _survivor.ReceiveWound();
+            _survivor.ReceiveWound();
+            
+            Assert.IsFalse(_survivor.isAlive);
+        }
+        
+        [Test]
+        public void HaveThreeActionsPerTurn()
+        {
+            Assert.AreEqual(3, _survivor.actions);
+        }
+    }
+}
